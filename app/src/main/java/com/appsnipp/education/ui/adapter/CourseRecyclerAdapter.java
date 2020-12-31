462439c632038c986aee20301cfcf989499fbd9c
Roger Colquehuanca Calcina <rogcolquehuancac@upt.pe>
2020-12-30 20:41:12 -0500

    viewbinding in adapters

--- app/src/main/java/com/appsnipp/education/ui/adapter/CourseRecyclerAdapter.java
+++ app/src/main/java/com/appsnipp/education/ui/adapter/CourseRecyclerAdapter.java
@@ -8,14 +8,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.appsnipp.education.R;
+import com.appsnipp.education.databinding.ItemCardBinding;
 import com.appsnipp.education.ui.listeners.CoursesItemClickListener;
 import com.appsnipp.education.ui.model.CourseCard;
 
@@ -36,20 +34,25 @@ public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAd
     @NonNull
     @Override
     public CourseRecyclerAdapter._ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.item_card,viewGroup,false);
-        return new _ViewHolder(view);
+//        View view = LayoutInflater.from(mContext).inflate(R.layout.item_card,viewGroup,false);
+//        return new _ViewHolder(view);
+
+        LayoutInflater layoutInflater= LayoutInflater.from(viewGroup.getContext());
+        ItemCardBinding itemCardBinding = ItemCardBinding.inflate(layoutInflater,viewGroup,false);
+        return new _ViewHolder(itemCardBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final CourseRecyclerAdapter._ViewHolder viewHolder, final int i) {
-        viewHolder.mItem = mData.get(i);
+//        viewHolder.mItem = mData.get(i);
         final int pos = viewHolder.getAdapterPosition();
         //Set ViewTag
         viewHolder.itemView.setTag(pos);
 
         viewHolder.setPostImage(mData.get(i));
-        viewHolder.course.setText(mData.get(i).getCourseTitle());
-        viewHolder.quantity_courses.setText(mData.get(i).getQuantityCourses());
+
+//        viewHolder.itemCardBinding.stagItemCourse.setText(mData.get(i).getCourseTitle());
+//        viewHolder.itemCardBinding.stagItemQuantityCourse.setText(mData.get(i).getQuantityCourses());
 
         //1st intent 2 methods
 //        if (i%2==1){
@@ -68,17 +71,17 @@ public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAd
 
             int dimenTopPixeles = getDimensionValuePixels(R.dimen.staggedmarginbottom);
             int dimenleftPixeles = getDimensionValuePixels(R.dimen.horizontal_card);
-            ViewGroup.MarginLayoutParams cardViewMarginParams = (ViewGroup.MarginLayoutParams) viewHolder.card_item.getLayoutParams();
+            ViewGroup.MarginLayoutParams cardViewMarginParams = (ViewGroup.MarginLayoutParams) viewHolder.itemCardBinding.cardItem.getLayoutParams();
 //            cardViewMarginParams.setMargins(dpToPx(8), dpToPx(20), 0, 0);
             cardViewMarginParams.setMargins(dimenleftPixeles, dimenTopPixeles, 0, 0);
-            viewHolder.card_item.requestLayout();
+            viewHolder.itemCardBinding.cardItem.requestLayout();
         }
 
 //      viewHolder.card_item.setBackgroundColor(mContext.getResources().getColor(R.color.color1));
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                coursesItemClickListener.onDashboardCourseClick(mData.get(i), viewHolder.imageView);
+                coursesItemClickListener.onDashboardCourseClick(mData.get(i), viewHolder.itemCardBinding.cardViewImage);
             }
         });
     }
@@ -116,22 +119,33 @@ public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAd
         return mData.size();
     }
 
-    public class _ViewHolder extends RecyclerView.ViewHolder{
-        ImageView imageView;
-        TextView course;
-        TextView quantity_courses;
-        CardView card_item;
-        public CourseCard mItem;
-        public _ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            card_item = itemView.findViewById(R.id.card_item);
-            imageView = itemView.findViewById(R.id.card_view_image);
-            course = itemView.findViewById(R.id.stag_item_course);
-            quantity_courses = itemView.findViewById(R.id.stag_item_quantity_course);
+    public static class _ViewHolder extends RecyclerView.ViewHolder{
+//        ImageView imageView;
+//        TextView course;
+//        TextView quantity_courses;
+//        CardView card_item;
+//        public CourseCard mItem;
+//        public _ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            card_item = itemView.findViewById(R.id.card_item);
+//            imageView = itemView.findViewById(R.id.card_view_image);
+//            course = itemView.findViewById(R.id.stag_item_course);
+//            quantity_courses = itemView.findViewById(R.id.stag_item_quantity_course);
+//        }
+
+        ItemCardBinding itemCardBinding;
+        public _ViewHolder(@NonNull ItemCardBinding cardBinding) {
+            super(cardBinding.getRoot());
+            this.itemCardBinding = cardBinding;
+
+            //this.itemRecyclerMealBinding.
         }
 
         void setPostImage(CourseCard courseCard){
-            imageView.setImageResource(courseCard.getImageCourse());
+            this.itemCardBinding.cardViewImage.setImageResource(courseCard.getImageCourse());
+            this.itemCardBinding.stagItemCourse.setText(courseCard.getCourseTitle());
+            this.itemCardBinding.stagItemQuantityCourse.setText(courseCard.getQuantityCourses());
         }
+
     }
 }
--- app/src/main/java/com/appsnipp/education/ui/adapter/MatchesCoursesAdapter.java
+++ app/src/main/java/com/appsnipp/education/ui/adapter/MatchesCoursesAdapter.java
@@ -7,13 +7,11 @@ package com.appsnipp.education.ui.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.appsnipp.education.R;
+import com.appsnipp.education.databinding.ItemShopCardBinding;
 import com.appsnipp.education.ui.listeners.MatchCourseClickListener;
 import com.appsnipp.education.ui.model.MatchCourse;
 import com.bumptech.glide.Glide;
@@ -33,30 +31,33 @@ public class MatchesCoursesAdapter extends RecyclerView.Adapter<MatchesCoursesAd
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View v = inflater.inflate(R.layout.item_shop_card, parent, false);
-        return new ViewHolder(v);
+//        View v = inflater.inflate(R.layout.item_shop_card, parent, false);
+//        return new ViewHolder(v);
+
+        ItemShopCardBinding itemCardBinding = ItemShopCardBinding.inflate(inflater,parent,false);
+        return new MatchesCoursesAdapter.ViewHolder(itemCardBinding);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        MatchCourse matchCourseDatos = mData.get(position);
+//        MatchCourse matchCourseDatos = mData.get(position);
+//        Glide.with(holder.itemView.getContext())
+//                .load(matchCourseDatos.getImageResource())
+////                .transform(new CenterCrop(), new RoundedCorners(24))
+////                .transform(new RoundedCorners(40))
+//                .into(holder.itemCardBinding.image);
 
-        holder.tv_titulo.setText(matchCourseDatos.getName());
-        holder.tv_cantidad_cursos.setText(matchCourseDatos.getNumberOfCourses());
-
-        Glide.with(holder.itemView.getContext())
-                .load(matchCourseDatos.getImageResource())
-//                .transform(new CenterCrop(), new RoundedCorners(24))
-//                .transform(new RoundedCorners(40))
-                .into(holder.image);
+        holder.setBind(mData.get(position));
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                matchCourseClickListener.onScrollPagerItemClick(matchCourseDatos, holder.image);
+                matchCourseClickListener.onScrollPagerItemClick(mData.get(position), holder.itemCardBinding.image);
             }
         });
+
     }
 
     @Override
@@ -66,15 +67,36 @@ public class MatchesCoursesAdapter extends RecyclerView.Adapter<MatchesCoursesAd
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private ImageView image;
-        private TextView tv_titulo;
-        private TextView tv_cantidad_cursos;
+//        private ImageView image;
+//        private TextView tv_titulo;
+//        private TextView tv_cantidad_cursos;
+//
+//        public ViewHolder(View itemView) {
+//            super(itemView);
+//            image = itemView.findViewById(R.id.image);
+//            tv_titulo = itemView.findViewById(R.id.tv_titulo);
+//            tv_cantidad_cursos = itemView.findViewById(R.id.tv_cantidad_cursos);
+//        }
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            image = itemView.findViewById(R.id.image);
-            tv_titulo = itemView.findViewById(R.id.tv_titulo);
-            tv_cantidad_cursos = itemView.findViewById(R.id.tv_cantidad_cursos);
+        ItemShopCardBinding itemCardBinding;
+        public ViewHolder(@NonNull ItemShopCardBinding cardBinding) {
+            super(cardBinding.getRoot());
+            this.itemCardBinding = cardBinding;
+
+            //this.itemRecyclerMealBinding.
+        }
+
+        void setBind(MatchCourse matchCourse){
+
+            itemCardBinding.tvTitulo.setText(matchCourse.getName());
+            itemCardBinding.tvCantidadCursos.setText(matchCourse.getNumberOfCourses());
+
+            Glide.with(itemView.getContext())
+                    .load(matchCourse.getImageResource())
+//                .transform(new CenterCrop(), new RoundedCorners(24))
+//                .transform(new RoundedCorners(40))
+                    .into(itemCardBinding.image);
         }
     }
+
 }
