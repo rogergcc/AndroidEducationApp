3b90710d6acb2f21e4685038a74de8f8d9bad221
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2023-06-17 23:55:43 -0500

    #Partial Implementation clean order some warnings missing inspect code..

--- app/src/test/java/com/appsnipp/education/ExampleUnitTest.java
+++ app/src/test/java/com/appsnipp/education/ExampleUnitTest.java
@@ -1,8 +1,9 @@
 package com.appsnipp.education;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
